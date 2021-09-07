b9fbf3d2edf48a3bf8b4c818cf38b783375fcd5d
Alena McLucas <alenamclucas@gmail.com>
2021-09-07 09:41:10 -0700

    adds CAST_MAP and value checks

--- ds_workflow/dataset/column_label.py
+++ ds_workflow/dataset/column_label.py
@@ -1,12 +1,60 @@
+import numpy as np
+import datetime as dt
 from collections import namedtuple
 
 
 AcceptableAttributes = namedtuple('Attribute', ['type', 'values'])
 
 COLUMN_VALUES = {
-    'category': AcceptableAttributes(str, ['categorical', 'numeric', 'date/time']),
-    'type': AcceptableAttributes(str, ['int', 'float', 'str', 'text', 'date', 'time', 'datetime']),
+    'category': AcceptableAttributes(str, ['categorical', 'numeric', 'text', 'date/time']),
+    'type': AcceptableAttributes(str, ['int', 'float', 'str', 'date', 'time', 'datetime']),
     'is_active': AcceptableAttributes(bool, [True, False])
+}
+
+NAME_TYPES_MAP = {'str': str, 'float': float, 'int': int, 'date': dt.date, 'time': dt.time, 'datetime': dt.datetime}
+
+# first element in 'category' will be default after data type successfully casted
+CAST_MAP = {
+    int: {
+        'category': ['numeric', 'categorical'],
+        'type': [float, dt.datetime]
+    },
+    np.int64: {
+        'category': ['numeric', 'categorical'],
+        'type': [float, dt.datetime]
+    },
+    float: {
+        'category': ['numeric'],
+        'type': [int]
+    },
+    np.float64: {
+        'category': ['numeric'],
+        'type': [int]
+    },
+    str: {
+        'category': ['categorical', 'text'],
+        'type': [int, float, dt.date, dt.time, dt.datetime]
+    },
+    object: {
+        'category': ['categorical', 'text'],
+        'type': [int, float, dt.date, dt.time, dt.datetime]
+    },
+    np.object_: {
+        'category': ['categorical', 'text'],
+        'type': [int, float, dt.date, dt.time, dt.datetime]
+    },
+    dt.date: {
+        'category': ['date/time'],
+        'type': [int]   # i.e. cast to number of days since x time
+    },
+    dt.time: {
+        'category': ['date/time'],
+        'type': [int]
+    },
+    dt.datetime: {
+        'category': ['date/time'],
+        'type': [int, dt.date, dt.time]
+    }
 }
 
 
@@ -16,16 +64,22 @@ class ColumnLabel:
     Attributes
     ----------
     category : str
-        Data category, can be 'categorical', 'numeric', or 'date/time'.
+        Data category, can be 'categorical', 'numeric', 'text', or 'date/time'.
     type : str
-        Data type, can be 'int', 'float', 'str', 'text', 'date', 'time', or 'datetime'.
+        Data type, can be 'int', 'float', 'str', 'date', 'time', or 'datetime'.
     is_active : bool
         If the column is enabled by the user.
+
+    Methods
+    -------
+    category_type_match()
+        Checks that the column's category and type are an acceptable match. Raises a ValueError if not.
     """
     def __init__(self, category, type, is_active):
         self.category = category
         self.type = type
         self.is_active = is_active
+        self.category_type_match()
 
     def __repr__(self):
         return f"category: {self.category}, type: {self.type}, is_active: {self.is_active}"
@@ -40,3 +94,8 @@ class ColumnLabel:
             raise ValueError(f"'{value}' must be an accepted value: {acceptable_values}.")
 
         self.__dict__[key] = value
+
+    def category_type_match(self):
+        categories_from_type = CAST_MAP[NAME_TYPES_MAP[self.type]]['category']
+        if self.category not in categories_from_type:
+            raise ValueError(f"'{self.category}' is not an acceptable category for data type '{self.type}'.")
--- tests/test_column_label.py
+++ tests/test_column_label.py
@@ -1,11 +1,14 @@
 from unittest import TestCase
 import numpy as np
+import datetime as dt
+from collections import namedtuple
+from itertools import chain
 
-from ds_workflow.dataset.column_label import ColumnLabel
+from ds_workflow.dataset.column_label import ColumnLabel, CAST_MAP, COLUMN_VALUES
 
 
-class TestValues(TestCase):
-
+class TestArguments(TestCase):
+    """Test arguments, ensuring they raise the correct exceptions when incorrect."""
     def test_base_case(self):
         base_case = ColumnLabel('categorical', 'str', True)
         result = str(base_case)
@@ -23,3 +26,25 @@ class TestValues(TestCase):
     def test_is_active_type(self):
         with self.assertRaises(ValueError):
             ColumnLabel(category='date/time', type='str', is_active=np.nan)
+
+    def test_category_type_match(self):
+        with self.assertRaises(ValueError):
+            ColumnLabel(category='categorical', type='datetime', is_active=False)
+
+
+class TestCastValues(TestCase):
+    """Check the developer's own work on the CAST_MAP variable, ensuring all values are acceptable."""
+    def test_categories(self):
+        coded_categories = set(chain.from_iterable([pairs['category'] for pairs in CAST_MAP.values()]))
+        accepted_categories = set(COLUMN_VALUES['category'].values)
+        coded_categories_diff = coded_categories.difference(accepted_categories)
+        self.assertEqual(len(coded_categories_diff), 0,
+                         msg=f"{coded_categories_diff} are not accepted categories.")
+
+    def test_types(self):
+        all_coded_types = chain.from_iterable([pairs['type'] for pairs in CAST_MAP.values()])
+        coded_types = set([t.__name__ for t in all_coded_types])
+        accepted_types = set(COLUMN_VALUES['type'].values)
+        coded_types_diff = coded_types.difference(accepted_types)
+        self.assertEqual(len(coded_types_diff), 0,
+                         msg=f"{coded_types_diff} are not accepted types.")
